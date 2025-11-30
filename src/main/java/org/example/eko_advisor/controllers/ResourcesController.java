5d1a88767da273c035c426552adf743aa02d5f55
Sergey <ushakov-01@mail.ru>
2025-11-30 20:30:26 +0300

    add resources, navigation and contacts

---
+++ src/main/java/org/example/eko_advisor/controllers/ResourcesController.java
@@ -0,0 +1,14 @@
+package org.example.eko_advisor.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/resources")
+public class ResourcesController {
+    @GetMapping
+    public String resources(){
+        return "resources";
+    }
+}
