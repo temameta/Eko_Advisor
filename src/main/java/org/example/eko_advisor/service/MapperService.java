055e9f1b29f250dabe90b95f63daa0a411e77be2
Tema <sezemov2@gmail.com>
2025-12-01 21:19:47 +0300

    deleted advice service and added general mapper service for all mappers

--- src/main/java/org/example/eko_advisor/service/MapperService.java
+++ src/main/java/org/example/eko_advisor/service/MapperService.java
@@ -2,12 +2,14 @@ package org.example.eko_advisor.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.eko_advisor.mappers.Advice;
+import org.example.eko_advisor.mappers.ESG;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 
 @Service
-public class AdviceService {
+public class MapperService {
     public Advice getAdvice() {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
@@ -15,6 +17,19 @@ public class AdviceService {
             return objectMapper.readValue(
                     new File("src/main/resources/data/advice.json"),
                     Advice.class
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public ESG getESG() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            // Чтение из файла
+            return objectMapper.readValue(
+                    new File("src/main/resources/data/esg.json"),
+                    ESG.class
             );
         } catch (Exception e) {
             e.printStackTrace();
