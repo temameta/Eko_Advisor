edb152546baba38146df8d2b9b789cb163ecc8e7
Tema <sezemov2@gmail.com>
2025-12-01 21:20:28 +0300

    refactored to mapper service

--- src/main/java/org/example/eko_advisor/controllers/AdviceController.java
+++ src/main/java/org/example/eko_advisor/controllers/AdviceController.java
@@ -1,6 +1,6 @@
 package org.example.eko_advisor.controllers;
 
-import org.example.eko_advisor.service.AdviceService;
+import org.example.eko_advisor.service.MapperService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,17 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/advice")
 public class AdviceController {
-    private final AdviceService adviceService;
+    private final MapperService mapperService;
 
     @Autowired
-    public AdviceController(AdviceService adviceService) {
-        this.adviceService = adviceService;
+    public AdviceController(MapperService mapperService) {
+        this.mapperService = mapperService;
     }
 
     @GetMapping
     public String getAdvice(Model model) {
-        System.out.println(adviceService.getAdvice());
-        model.addAttribute("advice", adviceService.getAdvice());
+        System.out.println(mapperService.getAdvice());
+        model.addAttribute("advice", mapperService.getAdvice());
         return "advice";
     }
 }
