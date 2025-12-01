c9554c60e9ee2f3991d846a981346c023306a1e9
Tema <sezemov2@gmail.com>
2025-12-01 16:24:19 +0300

    updated

--- src/main/java/org/example/eko_advisor/controllers/ESGController.java
+++ src/main/java/org/example/eko_advisor/controllers/ESGController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ESGController {
     @GetMapping
     public String getEsg() {
-        return "esg.html";
+        return "advice";
     }
 }
