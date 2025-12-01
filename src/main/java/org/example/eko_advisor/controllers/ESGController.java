a2194933feb96002c12a55a06cd5e501a598a11d
Tema <sezemov2@gmail.com>
2025-12-01 21:20:11 +0300

    added esg page

--- src/main/java/org/example/eko_advisor/controllers/ESGController.java
+++ src/main/java/org/example/eko_advisor/controllers/ESGController.java
@@ -1,14 +1,26 @@
 package org.example.eko_advisor.controllers;
 
+import org.example.eko_advisor.service.MapperService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/esg")
 public class ESGController {
+    private final MapperService mapperService;
+
+    @Autowired
+    public ESGController(MapperService mapperService) {
+        this.mapperService = mapperService;
+    }
+
     @GetMapping
-    public String getEsg() {
-        return "advice";
+    public String getEsg(Model model) {
+        model.addAttribute("esg", mapperService.getESG());
+        System.out.println(mapperService.getESG());
+        return "esg";
     }
 }
