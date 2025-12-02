1f304158ff0b6bde928ffd2430ca2cb6f371301e
Tema <sezemov2@gmail.com>
2025-12-02 16:22:21 +0300

    refactored contacts controllers

--- src/main/java/org/example/eko_advisor/controllers/ContactsController.java
+++ src/main/java/org/example/eko_advisor/controllers/ContactsController.java
@@ -1,30 +1,14 @@
 package org.example.eko_advisor.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.example.eko_advisor.mappers.Contacts;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.io.InputStream;
 
 @Controller
 @RequestMapping("/contacts")
 public class ContactsController {
     @GetMapping
-    public String getContacts(Model model) {
-        ObjectMapper mapper = new ObjectMapper();
-        Contacts contacts = null;
-        try (InputStream inputStream = getClass()
-                .getResourceAsStream("/data/contacts.json")) {
-            contacts = mapper.readValue(inputStream, Contacts.class);
-            model.addAttribute("creators", contacts.getCreators());
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public String getContacts() {
         return "contacts";
     }
 }
